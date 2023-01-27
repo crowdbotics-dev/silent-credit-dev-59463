80b8d5f1c75c5e91a91bf2867009fa5736302e2c
Crowdbotics <engineering@crowdbotics.com>
2023-01-27 00:05:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-credit-dev-59463",
+  url: "https://silent-credit-dev-59463.botics.co",
+  api: "https://silent-credit-dev-59463.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
